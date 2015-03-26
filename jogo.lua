5be76df170c5a6d6913a13984909c8a24884071c
fkas <fk.a316@gmail.com>
2015-03-26 00:34:01 -0300

    Adicionando Score
    1° teste

--- jogo.lua
+++ jogo.lua
@@ -11,11 +11,11 @@ mySong = audio.play( bgSound, { channel = 1, loops = -1 } )
 local physics = require("physics")
 physics.start()
 physics.setGravity( 0, 3 )
-physics.setDrawMode( "hybrid" )
+--physics.setDrawMode( "hybrid" )
 
 -- Cria a função criar paredes, regula a velocidade e uma variavel para atribuir os movimentos 
 function main( )
-	createWalls()
+    createWalls()
 end
 
 motionx = 0; 
@@ -44,6 +44,15 @@ local right = display.newImage( "images/right_button.png" )
 right.x = 270
 right.y = 265
 
+local score = 0
+
+local scoreNumber = display.newText(score, 400, 264, nil, 22)
+scoreNumber.xScale = 1.2
+scoreNumber.yScale = 1.2
+
+local scoreText = display.newText("score:", 320, 265, nil, 22)
+scoreText.xScale = 1.2
+
 -- Cria uma função para gerar varios objetos
 local function spawnBananas_g()
     local banana_g = display.newImage( "images/banana_g.png" )
@@ -59,7 +68,22 @@ local function spawnBananas_g()
        if ( event.phase == "began" ) then
           timer.performWithDelay(100, banana_gRemove)  
        end
+       if event.other == monkey then
+            
+            local function updateScore()
+            score = score + 1 
+            scoreNumber.text = score
+            end
+
+            timer.performWithDelay( 1, updateScore ) 
+
+            coinSound = audio.loadStream( "coin.mp3" )
+
+            mySong = audio.play( coinSound, { channel = 2, loops = 0 } )
+
+        end
     end
+
     banana_g.collision = onLocalCollision
     banana_g:addEventListener( "collision", banana_g )
 end    
@@ -88,7 +112,7 @@ timer.performWithDelay( 300, spawnBananas_b, 0 )
 
 -- Função para criar paredes
 function createWalls( )
-	
+    
      local wallThickenss = 10
 
      --left wall
@@ -101,31 +125,29 @@ function createWalls( )
 
 end
 
-main()	
+main()  
 
 -- Adiciona movimento do personagem de parada e de ir para a direita e esqueda
 local function stop (event)
-	if event.phase =="ended" then
-		motionx = 0;
-	end		
+    if event.phase =="ended" then
+        motionx = 0;
+    end     
 end
 Runtime:addEventListener("touch", stop )
 
 local function movemonkey (event)
-	monkey.x = monkey.x + motionx;
+    monkey.x = monkey.x + motionx;
 end
 Runtime:addEventListener("enterFrame", movemonkey)
  
 function left:touch()
-	motionx = -speed;
+    motionx = -speed;
 end
 left:addEventListener("touch",left)
 
 function right:touch()
-	motionx = speed;
+    motionx = speed;
 end
 right:addEventListener("touch",right)
 
 return scene
-
-
